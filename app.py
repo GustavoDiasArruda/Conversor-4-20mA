72a08a6150402a03022042bf333246162c732bd7
GustavoDiasArruda <gustavodarruda@hotmail.com>
2026-07-14 16:01:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pandas as pd  # <--- ADICIONE ESTA LINHA AQUI!
 
 st.set_page_config(page_title="Conversor de Instrumentação", page_icon="⚙️")
 # ... restante do seu código ...
-import streamlit as st
-
 st.set_page_config(page_title="Conversor de Instrumentação", page_icon="⚙️")
 
 st.title("⚙️ Conversor de Instrumentação")
