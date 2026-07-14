91d30f739327d5b632336af34566c50dfb487a13
GustavoDiasArruda <gustavodarruda@hotmail.com>
2026-07-14 14:44:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Conversor 4-20mA Pro", page_icon="⚡")
 
-st.title("⚡ TETEU")
+st.title("⚡ CONVERSOR 4-20mA")
 
 # --- PAINEL DE CONFIGURAÇÃO ---
 st.sidebar.header("Configurações da Escala")
